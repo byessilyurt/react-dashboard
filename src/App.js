ee44e5a938e91b26e052e480654a6a6fee63ba7c
yessilyurt <y.yesilyurt14@gmail.com>
2022-08-26 21:03:19 +0100

    Navbar created and styled

--- src/App.js
+++ src/App.js
@@ -23,9 +23,11 @@ import {
   Stacked,
 } from "./pages";
 
+import { useStateContext } from "./context/ContextProvider";
+
 import "./App.css";
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
 
   return (
     <div>
