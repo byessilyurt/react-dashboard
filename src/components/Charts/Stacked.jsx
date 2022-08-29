3cc705591f8563e5c1c6eba8b2a206a50c29f49e
yessilyurt <y.yesilyurt14@gmail.com>
2022-08-29 19:55:12 +0100

    Table, pagination, sorting added to Orders page

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -1,7 +1,40 @@
 import React from "react";
-
-const Stacked = () => {
-  return <div>Stacked</div>;
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  Legend,
+  Category,
+  StackingColumnSeries,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
+import {
+  stackedCustomSeries,
+  stackedPrimaryXAxis,
+  stackedPrimaryYAxis,
+} from "../../data/dummy";
+const Stacked = ({ width, height }) => {
+  return (
+    <ChartComponent
+      id="charts"
+      style={{ textAlign: "center" }}
+      width={width}
+      height={height}
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      tooltip={{ enable: true }}
+      legendSettings={{ background: "white" }}
+      chartArea={{ border: { width: 0 } }}
+    >
+      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+      <SeriesCollectionDirective>
+        {stackedCustomSeries.map((item, index) => {
+          <SeriesDirective key={index} {...item} />;
+        })}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  );
 };
 
 export default Stacked;
