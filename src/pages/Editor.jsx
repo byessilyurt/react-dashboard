4eefcf6356ecd944ed51610659357b0c7844743d
yessilyurt <y.yesilyurt14@gmail.com>
2022-08-29 21:28:17 +0100

    rich text editor added

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import {
+  HtmlEditor,
+  Toolbar,
+  Inject,
+  Image,
+  Link,
+  QuickToolbar,
+  RichTextEditorComponent,
+} from "@syncfusion/ej2-react-richtexteditor";
+
+import { EditorData } from "../data/dummy";
+import { Header } from "../components";
 
 const Editor = () => {
-  return <div>Editor</div>;
+  return (
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header title="Editor" category="App" />
+      <RichTextEditorComponent id="editor" value={EditorData}>
+        <EditorData />
+        <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]} />
+      </RichTextEditorComponent>
+    </div>
+  );
 };
 
 export default Editor;
